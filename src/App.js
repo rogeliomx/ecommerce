55324b6ceedfd670d0b750830e6c6eb413ae7a26
Rogelio Galindo <roge.galindo@gmail.com>
2021-12-26 20:47:42 -0600

    with switch 1st

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
 import Header from "./containers/Header";
-import ProductListing from './containers/ProductListing';
-import ProductDetails from './containers/ProductDetails';
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
-      <div className="App"> 
-        <Router>    
-                <Header />
-                <ProductListing />
-                  <Routes>
-                    <Route path="/" exact component={ProductListing} />
-                    
-                    <Route path="/product/:productId" component={ProductDetails} />
-                    <Route>404 Not Found!</Route>
-                  </Routes>
-                
-        </Router>
-{/* 
-        <Router>
-                <Header />
-                <Switch>
-                  <Route path="/" exact component={ProductListing} />
-                  <Route path="/product/:productId" component={ProductDetails} />
-                  <Route>404 Not Found!</Route>
-                </Switch>
-      </Router> */}
-
-      </div>
-  );  
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
