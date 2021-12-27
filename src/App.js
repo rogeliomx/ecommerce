d176223a5df97762c2643f30a2471fe5484148ba
Rogelio Galindo <roge.galindo@gmail.com>
2021-12-26 20:44:22 -0600

    funtinally normal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./containers/Header";
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is the new site for ecommerce 🏹 </h1>
-   
-    </div>
+      <div className="App"> 
+        <Router>    
+                <Header />
+                <ProductListing />
+                  <Routes>
+                    <Route path="/" exact component={ProductListing} />
+                    
+                    <Route path="/product/:productId" component={ProductDetails} />
+                    <Route>404 Not Found!</Route>
+                  </Routes>
+                
+        </Router>
+{/* 
+        <Router>
+                <Header />
+                <Switch>
+                  <Route path="/" exact component={ProductListing} />
+                  <Route path="/product/:productId" component={ProductDetails} />
+                  <Route>404 Not Found!</Route>
+                </Switch>
+      </Router> */}
+
+      </div>
   );  
 }
 
